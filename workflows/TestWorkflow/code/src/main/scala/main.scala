58f4dc6ba718f527028474be24180107f7a98fe5
Jane Doe <janedoe@example.org>
2021-01-06 08:27:07 +0000

    New Commit for Workflow Code

---
+++ workflows/TestWorkflow/code/src/main/scala/main.scala
@@ -0,0 +1,42 @@
+import io.prophecy.libs._
+import io.prophecy.libs.UDFUtils._
+import io.prophecy.libs.Component._
+import io.prophecy.libs.DataHelpers._
+import io.prophecy.libs.SparkFunctions._
+import io.prophecy.libs.FixedFileFormatImplicits._
+import org.apache.spark.sql.ProphecyDataFrame._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import config.ConfigStore._
+
+import graph._
+
+@Visual(mode = "batch")
+object Main {
+
+  def graph(spark: SparkSession): Unit = {
+
+    val df_Source0: Source = Source0(spark)
+
+  }
+
+  def main(args: Array[String]): Unit = {
+    import config._
+    ConfigStore.Config = ConfigurationFactoryImpl.fromCLI(args)
+
+    val spark: SparkSession = SparkSession
+      .builder()
+      .appName("TestWorkflow")
+      .config("spark.default.parallelism", 4)
+      .enableHiveSupport()
+      .getOrCreate()
+
+    val sc = spark.sparkContext
+    sc.setCheckpointDir("/tmp/checkpoints")
+
+    graph(spark)
+  }
+
+}
